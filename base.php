a1ac00cb5bc26f6a904c2c2b9bd3136e181d3223
Richard Lee <rlee@credly.com>
2017-12-04 15:23:52 -0500

    add authenticate function

--- base.php
+++ base.php
@@ -18,6 +18,32 @@ $middleware = new Oauth1([
 
 $stack->push($middleware);
 
+/**
+ * attempt to fetch token via CLE authentication
+ *
+ * @param      \GuzzleHttp\Client  $client  The client
+ * @param      array               $env     The environment
+ *
+ * @return     <type>              ( description_of_the_return_value )
+ */
+
+function authenticate( Client $client, array $env ){
+
+    $res = $client->post('cle_auth/authenticate', [
+        'auth' => 'oauth',
+        'form_params' => [
+            "user_id"      => $env['user_id'],
+            "integration_id" => $env['integration_id'],
+            "lis_person_contact_email_primary" => $env['email'],
+            "roles" => "ADMINISTRATOR",
+            "tool_consumer_info_product_family_code" => "cle",
+        ]
+    ]);
+
+    return json_decode($res->getBody());
+}
+
+
 return function($url) use ($env, $stack){
     return new Client([
         'base_uri' => $url,
